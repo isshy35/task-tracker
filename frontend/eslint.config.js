a6994de3a65ae8e36265c842a36a3dd6b2d4ae04
isshy <krissybowl@gmail.com>
2025-05-21 12:25:35 +0100

    added eslint.config

---
+++ frontend/eslint.config.js
@@ -0,0 +1,14 @@
+
+// eslint.config.js
+module.exports = {
+  extends: ["eslint:recommended"],
+  env: {
+    browser: true,
+    node: true
+  },
+  rules: {
+    "no-console": "warn",
+    "no-unused-vars": "warn"
+  }
+};
+
